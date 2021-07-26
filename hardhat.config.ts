2c7de2c4e57e56899d39dd3eed62a63c5ce01802
rlyx-31 <regina.liuyixuan@gmail.com>
2021-07-26 15:37:55 +0800

    add additional utilities

---
+++ hardhat.config.ts
@@ -0,0 +1,107 @@
+import "@nomiclabs/hardhat-waffle";
+import '@nomiclabs/hardhat-ethers';
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "@openzeppelin/hardhat-upgrades";
+
+import { resolve } from "path";
+import { config as dotenvConfig } from "dotenv";
+import { HardhatUserConfig, task } from "hardhat/config";
+import { NetworkUserConfig } from "hardhat/types";
+
+// Hardhat tasks
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// Setting up of Hardhat config
+dotenvConfig({ path: resolve(__dirname, "./.env") });
+
+const chainIds = {
+  ganache: 1337,
+  goerli: 5,
+  hardhat: 31337,
+  kovan: 42,
+  mainnet: 1,
+  rinkeby: 4,
+  ropsten: 3,
+};
+
+const mnemonic = process.env.MNEMONIC;
+if (!mnemonic) {
+  throw new Error("Please set your MNEMONIC in a .env file");
+}
+
+const infuraApiKey = process.env.INFURA_API_KEY;
+if (!infuraApiKey) {
+  throw new Error("Please set your INFURA_API_KEY in a .env file");
+}
+
+function createTestnetConfig(network: keyof typeof chainIds, networkAddress: string): NetworkUserConfig {
+  const url: string = "https://" + network + ".infura.io/v3/" + infuraApiKey;
+  return {
+    accounts: [networkAddress],
+    chainId: chainIds[network],
+    url,
+  };
+}
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  gasReporter: {
+    currency: "USD",
+    enabled: process.env.REPORT_GAS ? true : false,
+    excludeContracts: [],
+    src: "./contracts",
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic,
+      },
+      chainId: chainIds.hardhat,
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+        blockNumber: 12746317
+      }
+    },
+    ropsten: createTestnetConfig("ropsten", <string>process.env.ROPSTEN_PRIVATE_KEY)
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.2",
+        settings: {
+          metadata: {
+            bytecodeHash: "none",
+          },
+          optimizer: {
+            enabled: true,
+            runs: 800,
+          },
+        },
+      },
+      {
+        version: "0.5.12"
+      }
+    ]
+    
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
+};
+
+export default config;
